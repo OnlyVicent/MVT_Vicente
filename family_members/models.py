620b78a04ded3d3bfcaf855d13b26d5af3ad7432
Vicente Medina <vicentmm10@gmail.com>
2023-02-15 11:44:14 -0300

    commit 2

--- family_members/models.py
+++ family_members/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Family_members(models.Model):
-    name = models.CharField
-    age = models.IntegerField
+    name = models.CharField(max_length=50)
+    age = models.FloatField(null=True)
+    alive = models.IntegerField(null=True)
+
 
